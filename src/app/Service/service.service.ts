c78d30228ba559ab2aa2af8de492204142c1e03b
Carlos Miguel Rodriguez Botero <carlos.rodriguez1@utp.edu.co>
2024-07-24 14:55:23 -0500

    creacion de vista y funciones

---
+++ src/app/Service/service.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Persona } from '../Modelo/Persona';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServiceService {
+
+
+  constructor(private http:HttpClient) { }
+
+  Url='http://localhost:8080/crudProyect/personas';
+
+  getPersonas(){
+    return this.http.get<Persona[]>(this.Url)
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListarComponent } from './Persona/listar/listar.component';
+import { EditarComponent } from './Persona/editar/editar.component';
+import { AgregarComponent } from './Persona/agregar/agregar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListarComponent,
+    EditarComponent,
+    AgregarComponent
   ],
   imports: [
     BrowserModule,
